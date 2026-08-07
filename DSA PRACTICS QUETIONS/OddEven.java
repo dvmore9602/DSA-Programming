e7e95c7233d27875cb157040a7133d921b16aff6
Dnyaneshwari More <dvmore9602@gmail.com>
2026-08-07 14:15:06 +0530

    inital commit

---
+++ DSA PRACTICS QUETIONS/OddEven.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if(number % 2 == 0){
+            System.out.println("Is statment is Even");
+        
+        }
+        else{
+            System.out.println("Is Statement is Odd");
+        }
+
+        
+    }
+    
+}
