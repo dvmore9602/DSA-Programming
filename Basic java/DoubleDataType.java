a0d17a0f7b5f40b4e552aeba883a261af37e31d6
Dnyaneshwari More <dvmore9602@gmail.com>
2026-08-22 22:51:52 +0530

    Merge remote repository with local DSA code

---
+++ Basic java/DoubleDataType.java
@@ -0,0 +1,22 @@
+public class DoubleDataType {
+    public static void main(String[] args) {
+//         double x = 5;
+//         double y = 29;
+//         System.out.println(x - y);
+//         System.out.println(x + y);
+//         System.out.println(x * y);
+//         System.out.println(x / y );
+//     }
+// }
+
+// calculate the  persentage 5 subject
+double x1 = 89;
+double x2 = 90;
+double x3 = 87;
+double x4 = 94;
+double x5 = 98;
+
+double y = ( x1 + x2 + x3 + x4 +  x5) / 5;
+System.out.println(y);
+    }
+}
