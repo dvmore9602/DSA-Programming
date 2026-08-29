e5bf49bb5f1022fc37f71576759c410deff65a30
Dnyaneshwari More <dvmore9602@gmail.com>
2026-08-29 15:48:16 +0530

    t

---
+++ Loops/MultipleCoditions.java
@@ -0,0 +1,8 @@
+public class MultipleCoditions {
+
+    public static void main(String[] args) {
+        for(int i= 1, j=10; i<=10 && j>=6; i++,j--){
+            System.out.println(i + " " + j);
+        }
+    }
+}
---
+++ Loops/OddNo.java
@@ -0,0 +1,12 @@
+public class OddNo {
+    public static void main(String[] args) {
+
+        // output 1,3,5,7,9
+        // for(int i = 1; i<=10; i =i+2){
+        //     System.out.println(i);
+        // }
+        for(int i=1; i<= 100; i=i+1){
+            System.out.println(i);
+        }
+    }
+}
